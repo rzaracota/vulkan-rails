e085434db1373dc095170c8e52499956ce2c2c7d
Grey Gilroy <grey.gilroy@gmail.com>
2017-04-09 22:47:03 -0700

    Check validation layers

--- base_code.cpp
+++ base_code.cpp
@@ -3,21 +3,54 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+#include <cstring>
 #include <functional>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
+const std::vector<const char*> validationLayers = {
+  "VK_LAYER_LUNARG_standard_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+      bool layerFound = false;
+
+      for (const auto& layerProperties : availableLayers) {
+	std::cerr << "Layer name: " << layerProperties.layerName << std::endl;
+	
+	if (strcmp(layerName, layerProperties.layerName) == 0) {
+	  layerFound = true;
+	  break;
+	}
+      }
+
+      if (!layerFound) {
+	return false;
+      }
+    }
+
+    return true;
+}
+
 #include "vdeleter.h"
 
 class HelloTriangleApplication {
-public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-    }
-
 private:
     GLFWwindow* window;
 
@@ -30,8 +63,49 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
+  void createInstance() {
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+      throw std::runtime_error("validation layers requested but unavailable.");
+    } else {
+      throw std::runtime_error("validation layers present");
+    }
+    
+    VkApplicationInfo appInfo = {};
+
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    unsigned int glfwExtensionCount = 0;
+    const char ** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr,
+				       instance.replace());
+
+    if (result != VK_SUCCESS) {
+      throw std::runtime_error("Failed to create vulkan instance");
+    }                
+  }
+  
     void initVulkan() {
-        
+      createInstance();
+
+      
+      
     }
 
     void mainLoop() {
@@ -43,6 +117,17 @@ private:
 
         glfwTerminate();
     }
+
+  VDeleter<VkInstance> instance {vkDestroyInstance};
+
+  public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+    }
+
+
 };
 
 int main() {
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 cpp_flags = -g
 c++ = g++
 
-ld_flags = -lglfw
+ld_flags = -lglfw -lvulkan
 
 all: base_code.cpp
 	$(c++) $(cpp_flags) $(ld_flags) $< -o driver
