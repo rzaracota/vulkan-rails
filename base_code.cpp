527edd224ee2a0515ae8d27bc3ee5755e6279070
Grey Gilroy <grey.gilroy@gmail.com>
2017-04-09 23:03:42 -0700

    Setup debug callback

--- base_code.cpp
+++ base_code.cpp
@@ -48,12 +48,61 @@ bool checkValidationLayerSupport() {
     return true;
 }
 
+std::vector<const char*> getRequiredExtensions() {
+    std::vector<const char*> extensions;
+
+    unsigned int glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    for (unsigned int i = 0; i < glfwExtensionCount; i++) {
+        extensions.push_back(glfwExtensions[i]);
+    }
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+    }
+
+    return extensions;
+}
+
+VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback) {
+    static auto func = (PFN_vkCreateDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pCallback);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugReportCallbackEXT(VkInstance instance, VkDebugReportCallbackEXT callback, const VkAllocationCallbacks* pAllocator) {
+    static auto func = (PFN_vkDestroyDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT");
+    if (func != nullptr) {
+        func(instance, callback, pAllocator);
+    }
+}
+
 #include "vdeleter.h"
 
 class HelloTriangleApplication {
 private:
     GLFWwindow* window;
 
+  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+						      VkDebugReportFlagsEXT flags,
+						      VkDebugReportObjectTypeEXT objType,
+						      uint64_t obj,
+						      size_t location,
+						      int32_t code,
+						      const char* layerPrefix,
+						      const char* msg,
+						      void* userData) {
+
+    std::cerr << "validation layer: " << msg << std::endl;
+
+    return VK_FALSE;
+  }
+  
     void initWindow() {
         glfwInit();
 
@@ -66,8 +115,6 @@ private:
   void createInstance() {
     if (enableValidationLayers && !checkValidationLayerSupport()) {
       throw std::runtime_error("validation layers requested but unavailable.");
-    } else {
-      throw std::runtime_error("validation layers present");
     }
     
     VkApplicationInfo appInfo = {};
@@ -84,14 +131,17 @@ private:
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
-    unsigned int glfwExtensionCount = 0;
-    const char ** glfwExtensions;
+    auto extensions = getRequiredExtensions();
+    
+    createInfo.enabledExtensionCount = extensions.size();
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
-    createInfo.enabledLayerCount = 0;
+    if (enableValidationLayers) {
+      createInfo.enabledLayerCount = validationLayers.size();
+      createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+      createInfo.enabledLayerCount = 0;
+    }
 
     VkResult result = vkCreateInstance(&createInfo, nullptr,
 				       instance.replace());
@@ -100,12 +150,25 @@ private:
       throw std::runtime_error("Failed to create vulkan instance");
     }                
   }
+
+  void setupDebugCallback() {
+    if (!enableValidationLayers) return;
+
+    VkDebugReportCallbackCreateInfoEXT createInfo = {};
+
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+    createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+    createInfo.pfnCallback = debugCallback;
+
+    if (CreateDebugReportCallbackEXT(instance, &createInfo, nullptr,
+				     callback.replace()) != VK_SUCCESS) {
+      throw std::runtime_error("failed to set up debug callback!");
+    }
+  }
   
     void initVulkan() {
       createInstance();
-
-      
-      
+      setupDebugCallback();      
     }
 
     void mainLoop() {
@@ -119,6 +182,8 @@ private:
     }
 
   VDeleter<VkInstance> instance {vkDestroyInstance};
+  VDeleter<VkDebugReportCallbackEXT> callback{instance,
+      DestroyDebugReportCallbackEXT};
 
   public:
     void run() {
