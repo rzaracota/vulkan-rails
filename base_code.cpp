f0ee71f948bb5fd74f27827db71f5fc1bd73b9a1
Grey Gilroy <grey.gilroy@gmail.com>
2017-04-15 10:21:37 -0700

    Physical devices and queuefamilies

--- base_code.cpp
+++ base_code.cpp
@@ -165,10 +165,87 @@ private:
       throw std::runtime_error("failed to set up debug callback!");
     }
   }
+
+  bool isDeviceSuitable(VkPhysicalDevice device) {
+    VkPhysicalDeviceProperties deviceProperties;
+    VkPhysicalDeviceFeatures deviceFeatures;
+
+    vkGetPhysicalDeviceProperties(device, &deviceProperties);
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    QueueFamilyIndices indices = findQueueFamilies(device);
+    
+    return deviceProperties.deviceType ==
+      VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+      deviceFeatures.geometryShader &&
+      indices.isComplete();
+  }
+  
+  void pickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if (deviceCount == 0) {
+      throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto & device : devices) {
+      if (isDeviceSuitable(device)) {
+	physicalDevice = device;
+
+	break;
+      }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+  }
+
+  struct QueueFamilyIndices {
+    int graphicsFamily = -1;
+
+    bool isComplete() {
+      return graphicsFamily >= 0;
+    }
+  };
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+					     queueFamilies.data());
+
+    int i = 0;
+
+    for (const auto & queueFamily : queueFamilies) {
+      if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+	indices.graphicsFamily = i;
+      }
+
+      if (indices.isComplete()) {
+	break;
+      }
+
+      i++;
+    }
+    
+    return indices;
+  }
   
     void initVulkan() {
       createInstance();
-      setupDebugCallback();      
+      setupDebugCallback();
+      pickPhysicalDevice();
     }
 
     void mainLoop() {
@@ -181,6 +258,8 @@ private:
         glfwTerminate();
     }
 
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  
   VDeleter<VkInstance> instance {vkDestroyInstance};
   VDeleter<VkDebugReportCallbackEXT> callback{instance,
       DestroyDebugReportCallbackEXT};
