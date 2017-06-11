e4eed9a1575c19c993b95253e0c54e09c34a5260
Grey Gilroy <grey.gilroy@gmail.com>
2017-06-10 18:19:23 -0700

    use vertex buffers

--- base_code.cpp
+++ base_code.cpp
@@ -9,6 +9,49 @@
 #include <cstring>
 #include <functional>
 #include <algorithm>
+#include <array>
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+  glm::vec2 pos;
+  glm::vec3 color;
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription = {};
+
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof (Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 2>
+           getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 2>
+      attributeDescriptions = {};
+
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(Vertex, color);
+    
+    
+    return attributeDescriptions;
+  }
+};
+
+const std::vector<Vertex> vertices = {
+  {{ 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
+  {{ 0.5f, 0.5f }, { 0.0f, 1.0f,  0.0f }},
+  {{ -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }}
+};
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -670,10 +713,13 @@ private:
     vertexInputInfo.sType =
       VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
 
-    vertexInputInfo.vertexBindingDescriptionCount = 0;
-    vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
-    vertexInputInfo.vertexAttributeDescriptionCount = 0;
-    vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+    auto bindingDescription = Vertex::getBindingDescription();
+    auto attributeDescriptions = Vertex::getAttributeDescriptions();
+    
+    vertexInputInfo.vertexBindingDescriptionCount = 1;
+    vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+    vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
     VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
 
@@ -852,10 +898,66 @@ private:
 			    &commandPool) != VK_SUCCESS) {
       throw std::runtime_error("failed to create command pool!");
     }
+  }
+
+  uint32_t findMemoryType(uint32_t typeFilter,
+			  VkMemoryPropertyFlags properties) {
+    VkPhysicalDeviceMemoryProperties memProperties;
+
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+      if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+	return i;
+      }
+    }
+
+    throw std::runtime_error("failed to find suitable memory type!");
+  }
+  
+  void createVertexBuffer() {
+    VkBufferCreateInfo bufferInfo = {};
+
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+
+    bufferInfo.size = sizeof (vertices[0]) * vertices.size();
+
+    bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create vertex buffer!");
+    }
+
+    VkMemoryRequirements memRequirements;
+
+    vkGetBufferMemoryRequirements(device, vertexBuffer,
+				  &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo = {};
+
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+      throw std::runtime_error("Failed to allocate vertex buffer memory!");
+    }
+
+    vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+    void * data;
+
+    vkMapMemory(device, vertexBufferMemory, 0,
+		bufferInfo.size, 0, &data);
+
+    memcpy(data, vertices.data(), (size_t)bufferInfo.size);
+
+    vkUnmapMemory(device, vertexBufferMemory);
 
     
   }
-
+  
   void createCommandBuffers() {
     commandBuffers.resize(swapChainFramebuffers.size());
 
@@ -896,13 +998,21 @@ private:
 
       vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
 
-      vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+      VkBuffer vertexBuffers[] = { vertexBuffer };
+      VkDeviceSize offsets[] = { 0 };
+
+      vkCmdBindVertexBuffers(commandBuffers[i],
+			     0, 1, vertexBuffers, offsets);
+
+      vkCmdDraw(commandBuffers[i],
+		static_cast<uint32_t>(vertices.size()), 1, 0, 0);
 
       vkCmdEndRenderPass(commandBuffers[i]);
 
       if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
 	throw std::runtime_error("failed to record command buffer!");
       }
+
     }
   }
 
@@ -972,6 +1082,7 @@ private:
     createGraphicsPipeline();
     createFramebuffers();
     createCommandPool();
+    createVertexBuffer();
     createCommandBuffers();
     createSemaphores();
   }
@@ -1050,6 +1161,9 @@ private:
 
   void cleanup() {
     cleanupSwapChain();
+
+    vkDestroyBuffer(device, vertexBuffer, nullptr);
+    vkFreeMemory(device, vertexBufferMemory, nullptr);
     
     vkDestroySemaphore(device, renderFinishedSemaphore, nullptr);
     vkDestroySemaphore(device, imageAvailableSemaphore, nullptr);
@@ -1069,6 +1183,9 @@ private:
     glfwTerminate();
   }
 
+  VkBuffer vertexBuffer;
+  VkDeviceMemory vertexBufferMemory;
+  
   VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
   VkQueue graphicsQueue, presentQueue;
 
